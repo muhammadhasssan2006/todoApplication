bf5ba2a9059ecb402dea71b9ae79291dec639ec5
muhammadhasssan2006 <hassanweb365@gmail.com>
2025-11-02 15:00:06 +0500

    todo app complete

--- app.js
+++ app.js
@@ -1,85 +1,183 @@
+// Firebase Config
 const firebaseConfig = {
-  apiKey: "AIzaSyDUF4V2HxLmm6OW5YZcEu3LkzAb6aRLE4g",
-  authDomain: "todo-applicatioin.firebaseapp.com",
-  databaseURL: "https://todo-applicatioin-default-rtdb.firebaseio.com",
-  projectId: "todo-applicatioin",
-  storageBucket: "todo-applicatioin.firebasestorage.app",
-  messagingSenderId: "871188996213",
-  appId: "1:871188996213:web:02a38c1cd788b5bff2183c",
-  measurementId: "G-N1WMSL6TGY"
+  apiKey: "AIzaSyDiNW7M9v6n68MaF1xz8jl_7JBHO1XWllw",
+  authDomain: "todoapplication-4816e.firebaseapp.com",
+  projectId: "todoapplication-4816e",
+  storageBucket: "todoapplication-4816e.firebasestorage.app",
+  messagingSenderId: "278752416465",
+  appId: "1:278752416465:web:c83c8b24f186a1f99e6bf0",
+  measurementId: "G-3ZZKRBFMGF"
 };
+
 // Initialize Firebase
 var app = firebase.initializeApp(firebaseConfig);
-
 var db = firebase.database();
-
-
+console.log(app);
 
 var userInput = document.getElementById("todoInput");
-var todoList = document.getElementById("todoList");
 
-var todos = JSON.parse(localStorage.getItem("todos")) || [];
-showTodos();
-
+// Add Todo
 function addTodo() {
-  var text = userInput.value.trim();
-
-  if (text === "") {
-    alert("Please write something!");
-    return;
-  }
-
-  var todoObj = { text: text, done: false };
-  todos.push(todoObj);
-  saveTodos();
-  showTodos();
-    firebase.database().ref("todos").push(todoObj);
-  userInput.value = "";
-}
-
-function showTodos() {
-  todoList.innerHTML = "";
-
-  todos.forEach((todo, index) => {
-    var li = document.createElement("li");
-    li.className = "todo";
-
-    li.innerHTML = `
-      <input type="checkbox" id="todo-${index}" ${todo.done ? "checked" : ""}>
-      <label class="costum-checkbox" for="todo-${index}">
-        <svg fill="transparent" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24">
-          <path d="M382-240 154-468l57-57 171 171 367-367 57 57-424 424Z"/>
-        </svg>
-      </label>
-      <label for="todo-${index}" class="todo-text">${todo.text}</label>
-      <button class="delete-button">
-        <svg fill="var(--secondary-color)" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24">
-          <path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 
-          56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 
-          0h80v-360h-80v360ZM280-720v520-520Z"/>
-        </svg>
-      </button>
+  if (userInput.value === "") {
+    alert("Add something to write!");
+  } else {
+    var todoList = document.getElementById("list");
+    var liElement = document.createElement("li");
+    liElement.style.cssText = `
+      list-style-type: none; 
+      margin: 10px; 
+      background-color: #4A4D57; 
+      color: #f6f7ffff; 
+      padding: 15px; 
+      font-size: 18px; 
+      border: 2px solid #4A4D57; 
+      border-radius: 30px; 
+      display: flex; 
+      align-items: center;
+      justify-content: space-between;
     `;
 
-    var checkbox = li.querySelector("input");
-    checkbox.addEventListener("change", () => {
-      todos[index].done = checkbox.checked;
-      saveTodos();
-      showTodos();
+    var textSpan = document.createElement("span");
+    textSpan.style.flex = "1";
+    textSpan.style.wordBreak = "break-word"; 
+    textSpan.appendChild(document.createTextNode(userInput.value));
+    liElement.appendChild(textSpan);
+
+    var obj = { todo: userInput.value };
+    firebase.database().ref("todos").push(obj).then(function (response) {
+      var todoId = response.key;
+      liElement.setAttribute("id", todoId);
     });
 
-    var delBtn = li.querySelector(".delete-button");
-    delBtn.addEventListener("click", () => {
-      todos.splice(index, 1);
-      saveTodos();
-      showTodos();
-    });
+    // Buttons 
+    var btnWrapper = document.createElement("div");
+    btnWrapper.style.display = "flex";
+    btnWrapper.style.gap = "10px";
 
-    todoList.appendChild(li);
+    // Delete Button
+    var delBtn = document.createElement("button");
+    delBtn.innerText = "Delete";
+    delBtn.style.cssText = `
+      padding: 10px;
+      border: none; 
+      border-radius: 20px; 
+      background-color: #ff4757; 
+      box-shadow: 0 0 10px #ff4757;
+      cursor: pointer;
+    `;
+    delBtn.setAttribute("onclick", "deleteSingleItem(this)");
+
+    // Edit Button
+    var editBtn = document.createElement("button");
+    editBtn.innerText = "Edit";
+    editBtn.style.cssText = `
+      padding: 10px; 
+      border: none; 
+      border-radius: 20px; 
+      background-color: #00FFC4; 
+      box-shadow: 0 0 10px #00FFC4; 
+      cursor: pointer;
+    `;
+    editBtn.setAttribute("onclick", "editSingleItem(this)");
+
+    btnWrapper.appendChild(editBtn);
+    btnWrapper.appendChild(delBtn);
+    liElement.appendChild(btnWrapper);
+
+    todoList.appendChild(liElement);
+    userInput.value = "";
+  }
+}
+
+// get data from firebase
+function getDataFromDatabase() {
+  firebase
+    .database()
+    .ref("todos")
+    .once("value", function (snapshot) {
+      snapshot.forEach(function (childSnapshot) {
+        var data = childSnapshot.val();
+        var todoId = childSnapshot.key;
+
+        var todoList = document.getElementById("list");
+        var liElement = document.createElement("li");
+        liElement.setAttribute("id", todoId);
+        liElement.style.cssText = `
+          list-style-type: none; 
+          margin: 10px; 
+          background-color: #4A4D57; 
+          color: #f6f7ffff; 
+          padding: 15px; 
+          font-size: 18px; 
+          border: 2px solid #4A4D57; 
+          border-radius: 30px; 
+          display: flex; 
+          align-items: center;
+          justify-content: space-between;
+        `;
+
+        var textSpan = document.createElement("span");
+        textSpan.style.flex = "1";
+        textSpan.style.wordBreak = "break-word";
+        textSpan.appendChild(document.createTextNode(data.todo));
+        liElement.appendChild(textSpan);
+
+        var btnWrapper = document.createElement("div");
+        btnWrapper.style.display = "flex";
+        btnWrapper.style.gap = "10px";
+
+        var delBtn = document.createElement("button");
+        delBtn.innerText = "Delete";
+        delBtn.style.cssText = `
+          padding: 10px; 
+          border: none; 
+          border-radius: 20px; 
+          background-color: #ff4757; 
+          box-shadow: 0 0 10px #ff4757;
+          cursor: pointer;
+        `;
+        delBtn.setAttribute("onclick", "deleteSingleItem(this)");
+
+        var editBtn = document.createElement("button");
+        editBtn.innerText = "Edit";
+        editBtn.style.cssText = `
+          padding: 10px; 
+          border: none; 
+          border-radius: 20px; 
+          background-color: #00FFC4; 
+          box-shadow: 0 0 10px #00FFC4; 
+          cursor: pointer;
+        `;
+        editBtn.setAttribute("onclick", "editSingleItem(this)");
+
+        btnWrapper.appendChild(editBtn);
+        btnWrapper.appendChild(delBtn);
+        liElement.appendChild(btnWrapper);
+
+        todoList.appendChild(liElement);
+      });
+    });
+}
+getDataFromDatabase();
+
+// Delete Todo
+function deleteSingleItem(btn) {
+  var li = btn.parentNode.parentNode;
+  var todoId = li.getAttribute("id");
+
+  firebase.database().ref("todos/" + todoId).remove().then(function () {
+    li.remove();
   });
 }
 
-//  localStorage
-function saveTodos() {
-  localStorage.setItem("todos", JSON.stringify(todos));
+// Edit Todo
+function editSingleItem(btn) {
+  var li = btn.parentNode.parentNode;
+  var todoId = li.getAttribute("id");
+  var updatedValue = prompt("Enter updated value", li.childNodes[0].innerText);
+
+  if (updatedValue) {
+    firebase.database().ref("todos/" + todoId).update({ todo: updatedValue });
+    li.childNodes[0].innerText = updatedValue;
+  }
 }
